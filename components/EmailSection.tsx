8509d2acabb20bd9c8772252dfef60fa980f7532
Zeaforx <89166303+Zeaforx@users.noreply.github.com>
2024-09-09 10:26:14 +1300

    Update EmailSection.tsx

--- components/EmailSection.tsx
+++ components/EmailSection.tsx
@@ -71,11 +71,11 @@ const EmailSection = () => {
             <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
             <div className="z-10">
                 <h5 className="text-xl font-bold text-white my-2">
-                    Let&apos;s Meet
+                    Let's Meet
                 </h5>
                 <p className="text-[#ADB7BE] mb-4 max-w-md">
                     {" "}
-                    W&apos;e are currently looking for new opportunities, our
+                    We are currently looking for new opportunities, our
                     inbox are always open. We&apos;ll try my best to get back to
                     you!
                 </p>
